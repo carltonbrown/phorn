6e44a168443dfe7412d86bef0b1d1a61cef678c0
Carlton Brown <carlton.brown@salesloft.com>
2019-05-20 15:39:02 -0400

    remove unused var

--- lib/profile.ex
+++ lib/profile.ex
@@ -5,7 +5,7 @@ defmodule Profile do
     profile do
       count = 10000
       inspect_every = 100
-      phorns = Phorn.bulk_gen(9, count, MapSet.new([]), inspect_every)
+      Phorn.bulk_gen(9, count, MapSet.new([]), inspect_every)
     end
   end 
 end
